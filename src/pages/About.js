9bf00b6995712d605257dedc4dddd5b44cccfed0
cyril <telliercyril@gmail.com>
2024-08-04 17:30:12 +0200

    Route + Navigation

---
+++ src/pages/About.js
@@ -0,0 +1,54 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta dolorum
+        eum debitis beatae fugiat numquam dolores rem accusamus, deserunt modi
+        est ipsum, et nulla. Minus error esse quos officia inventore? Numquam
+        eveniet dolores nihil, ipsum enim soluta voluptatum beatae adipisci
+        porro sapiente blanditiis architecto quos cupiditate libero eligendi?
+        Animi minima porro dolorum sint dicta perspiciatis adipisci? Provident
+        magnam facilis odit! Alias officia voluptates nesciunt vel reiciendis
+        quasi ullam accusantium aliquam porro est modi eum facilis tempore
+        laudantium perspiciatis dignissimos, doloribus corrupti consectetur
+        dolorum nam? Omnis sunt nobis assumenda ipsum consectetur? Neque
+        architecto magni enim quos laborum ex unde assumenda dolorum, ab earum
+        sapiente distinctio odio tenetur facere fugit numquam dicta vero amet
+        magnam? Veritatis inventore maxime ullam? Libero, eos sequi! Natus
+        suscipit delectus tempore? Quia, harum libero dolorem in excepturi,
+        cupiditate sit obcaecati vitae illum itaque vel placeat magnam adipisci
+        atque ipsum, iure exercitationem. Maiores voluptates similique
+        cupiditate illo porro.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores
+        doloribus a libero quo quisquam, consequuntur veniam minima esse
+        possimus reiciendis error aliquam vel in nemo blanditiis rem minus!
+        Atque, odit! Deserunt ea aperiam porro officiis numquam, distinctio
+        delectus eaque molestias! Pariatur non obcaecati animi nostrum aut unde
+        id. Illo debitis impedit adipisci molestias nesciunt! Nemo nostrum
+        reprehenderit porro explicabo repellat! Eius dolorum a nihil sint
+        aspernatur, sunt nulla excepturi odio, dolores reiciendis quam
+        exercitationem delectus aliquid laudantium dicta recusandae animi
+        inventore eveniet explicabo provident omnis quisquam. Officia minus
+        minima excepturi. Ipsam deleniti quas dolorem vitae obcaecati cum
+        tenetur vel accusantium voluptas odit maxime, sapiente laborum, atque
+        culpa inventore non neque, nemo ab explicabo voluptates doloribus
+        molestiae sit itaque minus! Vero. Praesentium hic alias voluptatibus
+        pariatur minus, optio natus dignissimos iste numquam facilis voluptates
+        cum nemo ut sed odio ipsam eveniet magnam est provident molestias esse
+        quidem rerum corrupti! Aliquam, cumque?
+      </p>
+    </div>
+  );
+};
+
+export default About;
